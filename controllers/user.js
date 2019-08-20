c60755dcf23f072b1d50a39d91328dac3ac994ee
patrick <wyp4wj@gmail.com>
2019-08-20 14:41:40 -0700

    Implement hasAuth middleware and is_admin flag
    Lead routes are protected by hasAuth and only the admin user has
    access to them

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,9 @@ const passport = require('passport')
 const myPassport = require('../passport_setup')(passport)
 let flash = require('connect-flash')
 
+const { isEmpty } = require('lodash')
+const { validateUser } = require('../validators/signup')
+
 exports.show_login = function(req, res, next) {
   res.render('user/login', { formData: {}, errors: {} })
 }
@@ -12,21 +15,47 @@ exports.show_signup = function(req, res, next) {
   res.render('user/signup', { formData: {}, errors: {} })
 }
 
+const rerender_signup = function(errors, req, res, next) {
+  res.render('user/signup', { formData: req.body, errors: errors })
+}
+
 const generateHash = function(password) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
 }
 
 exports.signup = function(req, res, next) {
-  const newUser = models.User.build({
-    email: req.body.email,
-    password: generateHash(req.body.password)
-  })
-  return newUser.save().then(result => {
-    passport.authenticate('local', {
-      successRedirect: '/',
-      failureRedirect: '/signup',
-      failureFlash: true
-    })(req, res, next)
+  let errors = {}
+  return validateUser(errors, req).then(errors => {
+    if (!isEmpty(errors)) {
+      rerender_signup(errors, req, res, next)
+    } else {
+      return models.User.findOne({
+        where: {
+          is_admin: true
+        }
+      }).then(user => {
+        let newUser
+        if (user !== null) {
+          newUser = models.User.build({
+            email: req.body.email,
+            password: generateHash(req.body.password)
+          })
+        } else {
+          newUser = models.User.build({
+            email: req.body.email,
+            password: generateHash(req.body.password),
+            is_admin: true
+          })
+        }
+        return newUser.save().then(result => {
+          passport.authenticate('local', {
+            successRedirect: '/',
+            failureRedirect: '/signup',
+            failureFlash: true
+          })(req, res, next)
+        })
+      })
+    }
   })
 }
 
--- models/User.js
+++ models/User.js
@@ -27,6 +27,10 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
+    },
+    is_admin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true
     }
   })
 
